04280d45d294f732a7b000fb1333676a72449a58
Mumtaz Irteqa Ahmed <fareed2000ahmed@gmail.com>
2021-05-15 15:20:05 +0530

    proctoring added

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, Response
 from bson.objectid import ObjectId
 import pymongo
+import socket, cv2,pickle,struct,sys
+
+
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+def gen_frames():
+        #success, frame = camera.read()  # read the camera frame
+    client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    host_ip = '192.168.42.103' # paste your server ip address here
+    port = 5001
+    client_socket.connect((host_ip,port)) # a tuple
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4*1024) # 4K
+            if not packet:
+                break
+            data = data + packet
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q",packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += client_socket.recv(4*1024)
+        frame_data = data[:msg_size]
+        data  = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+    client_socket.close()
+
+@app.route('/proc')
+def proc():
+    return render_template('proc.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def hello_world():
@@ -103,4 +143,4 @@ def done():
     return render_template("done.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=6969)
